40273e432e9dd6d5ba8fc2aabf7c29a1a89013bb
chanveersingh <chanveersinghwork@gmail.com>
2025-03-23 11:52:29 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,30 @@
+
+wl ="welcome to calculator calculate the vale you want"
+opp = ("1.addition\n"
+       "2.subraction\n"
+       "3.division\n"
+       "4.multiplication\n"
+       "5.EXIT")
+y = "your result is :"
+print(wl)
+print(opp)
+
+option = int(input("enter your option from above:"))
+number1 = float(input("enter your first number:"))
+number2 = float(input("enteryour second nuber:"))
+# if else will give result with opratores of python
+if option == 1:
+    print(y,number1 + number2)
+elif option == 2:
+    print(y,number1 - number2)
+elif option == 3:
+    print(y,number2 / number1)
+elif option == 4:
+    print(y,number1*number2)
+elif option == 5:
+    print("bye bye have a nice day :)")
+    exit()
+
+else:
+    print("you value is invailed")
+
