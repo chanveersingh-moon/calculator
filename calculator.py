94f3c69efe5e9749c42b3bee6b36223dd2f9013c
chanveersingh <chanveersinghwork@gmail.com>
2025-03-25 13:41:18 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,10 +1,13 @@
-# this code is written by chanveersingh follow on g8thub for more
-wl ="welcome to calculator calculate the vale you want"
+# this code is written by chanveersingh follow on github for more
+
+wl ="welcome to calculator calculate the value you want"
 opp = ("1.addition\n"
        "2.subraction\n"
        "3.division\n"
        "4.multiplication\n"
-       "5.EXIT")
+       "5.float division\n"
+       "6.EXIT")
+
 y = "your result is :"
 print(wl)
 print(opp)
@@ -12,7 +15,9 @@ print(opp)
 option = int(input("enter your option from above:"))
 number1 = float(input("enter your first number:"))
 number2 = float(input("enteryour second nuber:"))
+
 # if else will give result with opratores of python
+
 if option == 1:
     print(y,number1 + number2)
 elif option == 2:
@@ -22,9 +27,10 @@ elif option == 3:
 elif option == 4:
     print(y,number1*number2)
 elif option == 5:
-    print("bye bye have a nice day :)")
+    print(y,number1//number2)
+elif option == 6:
+    print("bye bye have a nice day #")
     exit()
-
 else:
     print("you value is invailed")
 
